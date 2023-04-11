54a450fde060595bf2cdcab139d8bf50ac2e6172
Yunus Emre Hardal <hardaalyunusemre@gmail.com>
2023-04-11 14:54:54 +0300

    dbcontext

---
+++ src/Infrastructure/Data/ShopContext.cs
@@ -0,0 +1,31 @@
+﻿using ApplicationCore.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public class ShopContext : DbContext
+    {
+        public ShopContext(DbContextOptions<ShopContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories => Set<Category>();
+        public DbSet<Brand> Brands => Set<Brand>();
+        public DbSet<Product> Products => Set<Product>();
+
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // mevcut projedeki (Infrastructure) entity ayar dosyalarını bul ve uygula
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
